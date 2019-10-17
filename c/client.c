779b65ee82ce95f442d50de838fad0733c7aaf57
Chris West (Faux) <git@goeswhere.com>
2019-10-17 09:36:54 +0100

    handle errors

--- c/client.c
+++ c/client.c
@@ -6,7 +6,12 @@ extern uint32_t read_be_int();
 
 int main() {
     printf("Hello, world!\n");
-    uint32_t a = read_be_int();
-    printf("It was a %" PRIu32 "!\n", a);
+    int64_t a = read_be_int();
+    if (a < 0) {
+        fprintf(stderr, "Reading failed\n");
+    } else {
+        int32_t downcast = a;
+        printf("It was a %" PRIu32 "!\n", downcast);
+    }
 }
 
--- rs/src/lib.rs
+++ rs/src/lib.rs
@@ -5,6 +5,9 @@ use std::io;
 use byteorder::{BigEndian, ReadBytesExt};
 
 #[no_mangle]
-pub extern fn read_be_int() -> u32 {
-    io::stdin().read_u32::<BigEndian>().expect("couldn't read")
+pub extern fn read_be_int() -> i64 {
+    match io::stdin().read_u32::<BigEndian>() {
+        Ok(v) => i64::from(v),
+        Err(_) => -1,
+    }
 }
